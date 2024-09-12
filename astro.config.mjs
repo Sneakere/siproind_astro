f07f7917a9ef7e14e38f3dc9fcf668f04c08df12
Sebastian <ssalasg.mm@gmail.com>
2024-09-11 23:38:59 -0300

    xd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-const isProduction = process.env.NODE_ENV === 'production';
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: isProduction ? 'https://sneakere.github.io/siproind_astro/' : 'http://localhost:4321/',
-
+  site: "https://Sneakere.pages.github.io/",
+  base: "/siproind_astro"
 });
