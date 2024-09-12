7b6a162a58ec606ccda064118d50ce2636de56f2
Sebastian <ssalasg.mm@gmail.com>
2024-09-12 16:12:03 -0300

    mas efectos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -16,6 +16,10 @@ export default {
 				sans: ['Kanit', 'sans-serif'],
 				scrollBehavior: ['smooth'],
 			},
+			transitionDuration: {
+				'2000': '2000ms',
+				'3000': '3000ms'
+			}
 
 		},
 	},
