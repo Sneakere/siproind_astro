c6dfd10efd44d6ed07bf1dedba51c76a75b9db0d
Sebastian <ssalasg.mm@gmail.com>
2024-09-12 14:55:12 -0300

    algunas animaciones

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,15 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			keyframes: {
+				intro: {
+					'0%': { transform: 'translateY(-100%)' },
+					'100%': { transform: 'translateY(0)' },
+				},
+			},
+			animation: {
+				intro: 'intro 1s ease-out',
+			},
 			fontFamily: {
 				sans: ['Kanit', 'sans-serif'],
 				scrollBehavior: ['smooth'],
