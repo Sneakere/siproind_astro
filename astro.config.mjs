7b2f64529a8eac16fcaae4e484847bd667a5067c
Sebastian <ssalasg.mm@gmail.com>
2024-09-11 23:11:56 -0300

    jekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+const isProduction = process.env.NODE_ENV === 'production';
+const baseURL = '/';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: isProduction ? 'https://sneakere.github.io/siproind_astro/' : 'http://localhost:4321/',
+  base: baseURL
 });
